0da5475b5531185880c2c44d0effac098e17e96d
Kathy Reid <u6933485@anu.edu.au>
2019-05-07 22:48:05 +1000

    Add a file which displays the 3Ai logo on the Sense HAT LED display in an 8 x 8 grid

---
+++ 3Ailogo.py
@@ -0,0 +1,22 @@
+
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+# first row
+sense.set_pixel(0, 0, (0, 0, 0))
+sense.set_pixel(0, 1, (0, 0, 0))
+sense.set_pixel(0, 2, (0, 0, 0))
+sense.set_pixel(0, 3, (0, 0, 0))
+sense.set_pixel(0, 4, (0, 0, 0))
+sense.set_pixel(0, 5, (0, 0, 0))
+sense.set_pixel(0, 6, (0, 0, 0))
+sense.set_pixel(0, 7, (0, 0, 0))
+
+# second row
+
+
+
+sense.set_pixel(1, 2, (104, 15, 119))
+
+
